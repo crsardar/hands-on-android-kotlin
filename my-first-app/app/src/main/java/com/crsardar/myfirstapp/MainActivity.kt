97bca707916bdbc1f59772ae2d051cb1352168f3
Chittaranjan Sardar <crsardar@gmail.com>
2022-02-23 05:06:56 +0530

    Hands On Android: A simple animation in ConstraintLayout

--- my-first-app/app/src/main/java/com/crsardar/myfirstapp/MainActivity.kt
+++ my-first-app/app/src/main/java/com/crsardar/myfirstapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.crsardar.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.transition.TransitionManager
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.keyframe1)
+    }
+
+    fun onClick(view: View){
+
+        // A simple animation
+        val constraintLayout: ConstraintLayout = findViewById(R.id.constraint_layout)
+        val constraintSet = ConstraintSet()
+        constraintSet.load(this, R.layout.keyframe2)
+        TransitionManager.beginDelayedTransition(constraintLayout)
+        constraintSet.applyTo(constraintLayout)
     }
 }
